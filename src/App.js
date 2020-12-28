bc3d84f5db0f652297ad01e705d5635072f6f512
woody feng <woodyfeng@woodys-Mac-mini.local>
2020-12-27 19:23:17 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/test">kkkk ℹ️</a>
+        <p>
+          Hi there Hello world This is a new place
+        </p>
       </header>
     </div>
   );
