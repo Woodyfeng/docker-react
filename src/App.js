7b62b2f26f15b9e0ef0e2f9fe4bedffbd7a7425d
woody feng <woodyfeng@woodys-Mac-mini.local>
2020-12-28 02:23:59 -0500

    added travis file

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a href="/test">kkkk ℹ️</a>
-        <p>
-          Hi there Hello world This is a new place
-        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
